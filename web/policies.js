441c5c317dbf0b8fcf2331bd3b0466b35147e1f0
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-08-26 13:20:28 -0700

    Twitch Login!

---
+++ web/policies.js
@@ -0,0 +1,18 @@
+// Enter here the user flows and custom policies for your B2C application
+// To learn more about user flows, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-overview
+// To learn more about custom policies, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/custom-policy-overview
+
+const b2cPolicies = {
+    names: {
+        signUpSignIn: "B2C_1_Signup",
+        forgotPassword: "b2c_1_reset"
+    },
+    authorities: {
+        signUpSignIn: {
+            authority: "https://comfycorner.b2clogin.com/comfycorner.onmicrosoft.com/B2C_1_Signup",
+        },
+        forgotPassword: {
+            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_reset",
+        },
+    },
+}
